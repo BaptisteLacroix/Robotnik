38c94f89c35b0873bdcdece758e999c80058a84b
Baptiste Lacroix <baptiste.lacroix03@gmail.com>
2020-06-27 17:27:08 +0200

    add lib

--- Main.py
+++ Main.py
@@ -1,107 +1,104 @@
-import pygame
-from lib.pygame_functions import *
-import math
 from Game import Game
 from Monster import Pics
-from player import Player
+from lib.pygame_functions import *
 
 pygame.init()
 
-#charger le jeu
+# charger le jeu
 game = Game()
 
-#Affichage écran
+# Affichage écran
 pygame.display.set_caption("Sonic")
-screen = pygame.display.set_mode((game.W,game.H))
+screen = pygame.display.set_mode((game.W, game.H))
 
 background = pygame.image.load("img/Green_Hill.png")
 
 piegeBackground = Pics(game)
 
-#Position de l'image
+# Position de l'image
 x = 0
 
-#Coordonnées play_button
+# Coordonnées play_button
 play_button_x_min = 371
 play_button_x_max = 648
 play_button_y_min = 249
 play_button_y_max = 347
 
-#importer/charger la bannière
+# importer/charger la bannière
 banner = pygame.image.load("img/Banniere/banner0.jpg")
-banner = pygame.transform.scale(banner, (game.W,game.H))
+banner = pygame.transform.scale(banner, (game.W, game.H))
 banner_rect = banner.get_rect()
 
 #########
 
 running = True
 
-
 #################
-testSprite = makeSprite("img/running/SonicSprite_resized.gif", 32)
+testSprite = makeSprite("img/running/SonicSprite_resized.gif", 31)
 
-moveSprite(testSprite,300,300,True)
+moveSprite(testSprite, 300, 300, True)
 showSprite(testSprite)
 
 nextFrame = clock()
 frame = 0
 
-
-#Boucle tant que condition est vraie
+# Boucle tant que condition est vraie
 while running:
 
-	if clock() > nextFrame:
-		frame = (frame+1)%8
-		nextFrame += 80
+    if clock() > nextFrame:
+        frame = (frame + 1) % 8
+        nextFrame += 80
 
-	if keyPressed(K_d):
-		changeSpriteImage(testSprite, 0*8+frame)
+    if keyPressed(pygame.K_d):
+        changeSpriteImage(testSprite, 0 * 8 + frame)
 
-	elif keyPressed(K_q):
-		changeSpriteImage(testSprite, 2*8+frame)
+    elif keyPressed(pygame.K_q):
+        changeSpriteImage(testSprite, 2 * 8 + frame)
 
-	else:
-		game.player.image = pygame.image.load("img/SonicStatiqueRight.png")
+    else:
+        game.player.image = pygame.image.load("img/SonicStatiqueRight.png")
 
-	#Appliquer l'arrère plan et le faire bouger
-	relative_x = game.background_origin_x % background.get_rect().width
-	screen.blit(background, (relative_x - background.get_rect().width,0))
-	if relative_x < game.W:
-			screen.blit(background,(relative_x,0))
+    tick(120)
 
-	#vérifier si le jeu a commencé
-	if game.is_playing:
-		#délencher les instructions de la partie
-		game.update(screen)
-	#vérifier si le jeu n'a pas commencé
-	else:
-		#ajouter l'écran de bienvenue
-		screen.blit(banner, banner_rect)
+    # Appliquer l'arrère plan et le faire bouger
+    relative_x = game.background_origin_x % background.get_rect().width
+    screen.blit(background, (relative_x - background.get_rect().width, 0))
+    if relative_x < game.W:
+        screen.blit(background, (relative_x, 0))
 
-	#Mise a jour de la fenêtre
-	pygame.display.flip()
+    # vérifier si le jeu a commencé
+    if game.is_playing:
+        # délencher les instructions de la partie
+        game.update(screen)
+    # vérifier si le jeu n'a pas commencé
+    else:
+        # ajouter l'écran de bienvenue
+        screen.blit(banner, banner_rect)
 
-	#Si le joueur ferme la fenêtre
-	for event in pygame.event.get():
-		#vérifier la fermeture de la fenêtre
-		if event.type == pygame.QUIT:
-			running = False
-			pygame.quit()
+    # Mise a jour de la fenêtre
+    pygame.display.flip()
 
-		#detecter si une touche du clavier est laché
-		elif event.type == pygame.KEYDOWN:
-			game.pressed[event.key] = True
+    # Si le joueur ferme la fenêtre
+    for event in pygame.event.get():
+        # vérifier la fermeture de la fenêtre
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
 
-			#détecter si la touche espace est enclanchée pour lancer le projectile
-			if event.key == pygame.K_SPACE:
-				game.player.launch_projectile()
+        # detecter si une touche du clavier est laché
+        elif event.type == pygame.KEYDOWN:
+            game.pressed[event.key] = True
 
-		elif event.type == pygame.KEYUP:
-			game.pressed[event.key] = False
+            # détecter si la touche espace est enclanchée pour lancer le projectile
+            if event.key == pygame.K_SPACE:
+                game.player.launch_projectile()
 
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			x_mouse, y_mouse = pygame.mouse.get_pos()
-			#Vérification si clique sur le bouton
-			if x_mouse > play_button_x_min and x_mouse < play_button_x_max and y_mouse < play_button_y_max and y_mouse > play_button_y_min :
-				#mettre le jeu en mode "lancé"
-				game.start()
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            # Vérification si clique sur le bouton
+            if play_button_x_min < x_mouse < play_button_x_max and play_button_y_max > y_mouse > play_button_y_min:
+                # mettre le jeu en mode "lancé"
+                game.start()
--- player.py
+++ player.py
@@ -2,60 +2,54 @@ import pygame
 from projectile import Projectile
 
 
-#jump_height = 50
+# jump_height = 50
 
-#Joueur Sonic
+# Joueur Sonic
 class Player(pygame.sprite.Sprite):
 
-	def __init__(self, game):
-		super().__init__()
-		self.game = game
-		self.attack = 50
-		self.health = 102
-		self.max_health = 102
-		self.velocity = 2
-		self.all_projectiles = pygame.sprite.Group()
-		self.image = pygame.image.load("img/SonicStatiqueRight.png")
-		self.rect = self.image.get_rect()
-		self.rect.x = 50
-		self.rect.y = 200
-		#self.jumping = False
-		#self.jump_offset = 0
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.attack = 50
+        self.health = 102
+        self.max_health = 102
+        self.velocity = 2
+        self.all_projectiles = pygame.sprite.Group()
+        self.image = pygame.image.load("img/SonicStatiqueRight.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 50
+        self.rect.y = 200
 
-	def damage(self, amount):
-		if self.health - amount > amount:
-			self.health -= amount
-		else:
-			#si le joueur n'a plus de points de vie
-			self.game.game_over()
+    # self.jumping = False
+    # self.jump_offset = 0
 
+    def damage(self, amount):
+        if self.health - amount > amount:
+            self.health -= amount
+        else:
+            # si le joueur n'a plus de points de vie
+            self.game.game_over()
 
-	def update_health_bar(self, surface):
-		#dessiner la barre de vie
-		pygame.draw.rect(surface, (60, 63, 60), [self.rect.x + 15, self.rect.y - 20, self.max_health, 7])
-		pygame.draw.rect(surface, (229, 25, 25), [self.rect.x + 15, self.rect.y - 20, self.health, 7])
+    def update_health_bar(self, surface):
+        # dessiner la barre de vie
+        pygame.draw.rect(surface, (60, 63, 60), [self.rect.x + 15, self.rect.y - 20, self.max_health, 7])
+        pygame.draw.rect(surface, (229, 25, 25), [self.rect.x + 15, self.rect.y - 20, self.health, 7])
 
+    def launch_projectile(self):
+        # créer une nouvelle instance de la classe projectile
+        self.all_projectiles.add(Projectile(self))
 
-	def launch_projectile(self):
-		#créer une nouvelle instance de la classe projectile
-		self.all_projectiles.add(Projectile(self))
+    def move_right(self):
+        if not self.game.check_collision(self, self.game.all_pics):
+            self.rect.x += self.velocity
 
+    def move_left(self):
+        self.rect.x -= self.velocity
 
-	def move_right(self):
-		if not self.game.check_collision(self, self.game.all_pics):
-			self.rect.x += self.velocity
+    def move_up(self):
+        self.rect.y -= self.velocity
 
-	def move_left(self):
-		self.rect.x -= self.velocity
-
-	def move_up(self):
-		self.rect.y -= self.velocity
-
-
-
- 
-
-	"""
+    """
 
 	def do_jumping(Player):
 		global jump_height
