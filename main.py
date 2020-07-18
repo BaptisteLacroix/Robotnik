720f71f3f863d6846c49e4b06f3149d24a8b4e5d
Baptiste Lacroix <baptiste.lacroix03@gmail.com>
2020-07-18 18:06:40 +0200

    Update main

--- main.py
+++ main.py
@@ -1,99 +1,18 @@
 from party import Party
-from monster import Pic
-from player import Player
 from menu import Menu
 import pygame
 
-pygame.init()
 
-# Affichage écran
-pygame.display.set_caption("Sonic")
-screen = pygame.display.set_mode((Party.W, Party.H))
-print(screen)
-
-#background = pygame.image.load("img/Green_Hill.png")
-
-
-
-# Position de l'image
-x = 0
-
-##############
-
-# charger le jeu
-menu = Menu(screen)
-
-###########
-piegeBackground = Pic()
-
-#lance le menu
-menu.loop()
-
-###############
-
-frame = 0
-
-# Boucle tant que condition est vraie
-while party.running:
-    pass
-
-#################
-"""
-frame = 0
-
-# Boucle tant que condition est vraie
-while game.running:
-
-    keys = pygame.key.get_pressed()
-    game.player.process_movement_animation(keys)
-
-    # Appliquer l'arrère plan et le faire bouger
-    relative_x = game.background_origin_x % background.get_rect().width
-    screen.blit(background, (relative_x - background.get_rect().width, 0))
-    if relative_x < game.W:
-        screen.blit(background, (relative_x, 0))
-
-    # vérifier si le jeu a commencé
-    if game.is_playing:
-        # délencher les instructions de la partie
-        game.update(screen)
-    # vérifier si le jeu n'a pas commencé
-    else:
-        # ajouter l'écran de bienvenue
-        screen.blit(banner, banner_rect)
+def main():
+    pygame.init()
+    # Affichage écran
+    pygame.display.set_caption("Sonic")
+    screen = pygame.display.set_mode((Party.W, Party.H))
+    # charger le jeu
+    menu = Menu(screen)
+    # lance le menu
+    menu.loop()
 
 
-    # Mise a jour de la fenêtre
-    pygame.display.flip()
-
-    for event in pygame.event.get():
-        # vérifier la fermeture de la fenêtre
-        if event.type == pygame.QUIT:
-            game.is_playing = False
-            pygame.quit()
-
-        # detecter si une touche du clavier est laché
-        elif event.type == pygame.KEYDOWN:
-            game.pressed[event.key] = True
-
-            if event.key == pygame.K_SPACE:
-                print("saut")
-                Player.jump(player)
-
-            if event.key == pygame.K_ESCAPE:
-                game.is_playing = False
-                screen.blit(banner, banner_rect)
-
-        elif event.type == pygame.KEYUP:
-            game.pressed[event.key] = False
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x_mouse, y_mouse = pygame.mouse.get_pos()
-            # Vérification si clique sur le bouton
-            if not game.is_playing and play_button_x_min < x_mouse < play_button_x_max and play_button_y_max > y_mouse > play_button_y_min:
-                # mettre le jeu en mode "lancé"
-                game.start()
-            # détecter si le click gauche est enclanché pour lancer le projectile
-            elif game.is_playing and pygame.mouse.get_pressed()[0]:
-                game.player.launch_projectile()
-"""
+if __name__ == '__main__':
+    main()
